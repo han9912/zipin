1937069568286188893820a5d011d46554330d82
han9912 <han9912@outlook.com>
2025-04-30 22:42:11 -0400

    Initial commit

---
+++ src/main/java/io/github/han9912/zipin/ZipinApplication.java
@@ -0,0 +1,13 @@
+package io.github.han9912.zipin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipinApplication.class, args);
+	}
+
+}
