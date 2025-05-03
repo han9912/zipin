d9c061083bae5d80803b3ecc646e1002d5f88580
han9912 <han9912@outlook.com>
2025-05-03 13:04:28 -0400

    麻了 。

--- src/test/java/io/github/han9912/zipin/ZipinApplicationTests.java
+++ src/test/java/io/github/han9912/zipin/ZipinApplicationTests.java
@@ -2,8 +2,10 @@ package io.github.han9912.zipin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZipinApplicationTests {
 
 	@Test
