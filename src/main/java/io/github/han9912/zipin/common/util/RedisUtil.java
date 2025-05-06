502142f7aa7108e399c2ca4262f0f69143d1309f
han9912 <han9912@outlook.com>
2025-05-05 22:59:07 -0400

    Redis config and util added

---
+++ src/main/java/io/github/han9912/zipin/common/util/RedisUtil.java
@@ -0,0 +1,23 @@
+package io.github.han9912.zipin.common.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtil {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public void set(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void del(String key) {
+        redisTemplate.delete(key);
+    }
+}
