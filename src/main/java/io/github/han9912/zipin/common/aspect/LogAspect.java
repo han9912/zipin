fb56ec2e8abc3b8fc102a88cd18aab07a918792a
han9912 <han9912@outlook.com>
2025-05-06 21:06:44 -0400

    Enhance Log Aspect

--- src/main/java/io/github/han9912/zipin/common/aspect/LogAspect.java
+++ src/main/java/io/github/han9912/zipin/common/aspect/LogAspect.java
@@ -1,8 +1,12 @@
 package io.github.han9912.zipin.common.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -10,10 +14,21 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class LogAspect {
-    private final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogAspect.class);
+    private final Logger log = org.slf4j.LoggerFactory.getLogger(LogAspect.class);
 
-    @Before("execution(* io.github.han9912..controller..*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        log.info("调用方法：{}， 参数：{}", joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()));
+    @Pointcut("execution(* io.github.han9912..service..*(..))")
+    public void logPointCut() {}
+
+    @Around("logPointCut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        log.info("调用方法：{}，参数：{}", joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()));
+
+        Object result = joinPoint.proceed();
+
+        long elapsed = System.currentTimeMillis() - start;
+        log.info("返回结果：{}，耗时：{} ms", result, elapsed);
+
+        return result;
     }
 }
