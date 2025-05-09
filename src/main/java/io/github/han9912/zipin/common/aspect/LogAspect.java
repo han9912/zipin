269ef299a14650b1072ba27835f227f129addff0
han9912 <han9912@outlook.com>
2025-05-09 18:37:21 -0400

    remove useless import

--- src/main/java/io/github/han9912/zipin/common/aspect/LogAspect.java
+++ src/main/java/io/github/han9912/zipin/common/aspect/LogAspect.java
@@ -1,15 +1,13 @@
 package io.github.han9912.zipin.common.aspect;
 
-import org.aspectj.lang.JoinPoint;
+import java.util.Arrays;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Aspect
 @Component
@@ -17,7 +15,8 @@ public class LogAspect {
     private final Logger log = org.slf4j.LoggerFactory.getLogger(LogAspect.class);
 
     @Pointcut("execution(* io.github.han9912..service..*(..))")
-    public void logPointCut() {}
+    public void logPointCut() {
+    }
 
     @Around("logPointCut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
--- src/main/java/io/github/han9912/zipin/user/util/JwtUtil.java
+++ src/main/java/io/github/han9912/zipin/user/util/JwtUtil.java
@@ -1,17 +1,17 @@
 package io.github.han9912.zipin.user.util;
 
-import io.github.han9912.zipin.user.entity.User;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.Jwts;        // Jwts 是生成和解析 JWT 的主类
-import io.jsonwebtoken.SignatureAlgorithm; // 用于指定加密算法
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
+import io.github.han9912.zipin.user.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
