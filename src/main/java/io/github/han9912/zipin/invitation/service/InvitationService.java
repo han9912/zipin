f7ed3d2e3db2e34d56a003490619087838fa52cf
han9912 <han9912@outlook.com>
2025-05-15 10:55:32 -0400

    Remove public in service

--- src/main/java/io/github/han9912/zipin/invitation/service/InvitationService.java
+++ src/main/java/io/github/han9912/zipin/invitation/service/InvitationService.java
@@ -4,5 +4,5 @@ import io.github.han9912.zipin.invitation.dto.InvitationRequest;
 import io.github.han9912.zipin.invitation.entity.Invitation;
 
 public interface InvitationService {
-    public Invitation invite(InvitationRequest request, Long recruiterId);
+    Invitation invite(InvitationRequest request, Long recruiterId);
 }
